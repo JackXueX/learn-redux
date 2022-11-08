b0a57ec6a22db7ee051af1d041d828f711b9f938
JackXueX <42790406+JackXueX@users.noreply.github.com>
2022-11-08 20:45:43 +0800

    异步请求

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,14 +1,96 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
 
-const initialState = [
-  { id: '1', title: 'First Post!', content: 'Hello!' },
-  { id: '2', title: 'Second Post', content: 'More text' }
-]
+const initialState = {
+  posts: [],
+  status: 'idle',
+  error: null
+}
+
+export const addNewPost = createAsyncThunk(
+  'posts/addNewPost',
+  // payload 创建者接收部分“{title, content, user}”对象
+  async initialPost => {
+    // 我们发送初始数据到 API server
+    const response = await client.post('/fakeApi/posts', initialPost)
+    // 响应包括完整的帖子对象，包括唯一 ID
+    return response.data
+  }
+)
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await client.get('/fakeApi/posts')
+  return response.data
+})
 
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
-  reducers: {}
+  reducers: {
+    // postAdded: {
+    //   reducer(state, action) {
+    //     state.push(action.payload)
+    //   },
+    //   prepare(title, content, userId) {
+    //     return {
+    //       payload: {
+    //         id: nanoid(),
+    //         date: new Date().toISOString(),
+    //         title,
+    //         content,
+    //         user: userId,
+    //         reactions: {
+    //           thumbsUp: 0,
+    //           hooray: 0,
+    //           heart: 0,
+    //           rocket: 0,
+    //           eyes: 0,
+    //         },
+    //       },
+    //     }
+    //   },
+    // },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find((post) => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
+    },
+    postUpdated(state, action) {
+      const { id, title, content } = action.payload
+      const existingPost = state.find((post) => post.id === id)
+      if (existingPost) {
+        existingPost.title = title
+        existingPost.content = content
+      }
+    },
+  },
+
+  extraReducers(builder) {
+    builder.addCase(fetchPosts.pending, (state, action) => {
+      state.status = "loading"
+    })
+    .addCase(fetchPosts.fulfilled, (state, action) => {
+      state.status = 'succeeded'
+      // Add any fetched posts to the array
+      state.posts = state.posts.concat(action.payload)
+    })
+    .addCase(fetchPosts.rejected, (state, action) => {
+      state.status = 'failed'
+      state.error = action.error.message
+    }).addCase(addNewPost.fulfilled, (state, action) => {
+      // 我们可以直接将新的帖子对象添加到我们的帖子数组中
+      state.posts.push(action.payload)
+    })
+  }
 })
 
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
+
 export default postsSlice.reducer
+
+export const selectAllPosts = state => state.posts.posts
+
+export const selectPostById = (state, postId) =>
+  state.posts.find(post => post.id === postId)
